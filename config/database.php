591d83af9deffe83c0b247f4048246f1ace9dfae
Clinton Wright <cwright@hilt-cwright.bwt>
2015-09-30 17:14:05 +0200

    Initial commit 2. Getting structure in place

--- config/database.php
+++ config/database.php
@@ -55,9 +55,9 @@ return [
 		'mysql' => [
 			'driver'    => 'mysql',
 			'host'      => env('DB_HOST', 'localhost'),
-			'database'  => env('DB_DATABASE', 'forge'),
-			'username'  => env('DB_USERNAME', 'forge'),
-			'password'  => env('DB_PASSWORD', ''),
+			'database'  => env('DB_DATABASE', 'bwt_dev_hub'),
+			'username'  => env('DB_USERNAME', 'dev_admin'),
+			'password'  => env('DB_PASSWORD', 'Cwright2207'),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
